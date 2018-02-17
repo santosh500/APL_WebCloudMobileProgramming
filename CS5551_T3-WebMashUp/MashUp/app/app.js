0d50a65a853649ede6965bfd78d48578f696630e
Paul Charles <santosh41690@gmail.com>
2018-02-17 02:48:00 -0600

    commit 3

---
+++ CS5551_T3-WebMashUp/MashUp/app/app.js
@@ -0,0 +1,49 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+angular.module('myApp', [])
+
+
+    .controller('View1Ctrl', function ($scope, $http) {
+        $scope.venueList = new Array();
+        $scope.mostRecentReview;
+        $scope.getVenues = function () {
+            var placeEntered = document.getElementById("txt_placeName").value;
+            var searchQuery = document.getElementById("txt_searchFilter").value;
+            if (placeEntered != null && placeEntered != "" && searchQuery != null && searchQuery != "") {
+                document.getElementById('div_ReviewList').style.display = 'none';
+                //This is the API that gives the list of venues based on the place and search query.
+                var handler = $http.get("https://api.foursquare.com/v2/venues/search" +
+                    "?client_id=Q0ENF1YHFTNPJ31DCF13ALLENJW0P5MTH13T1SA0ZP1MUOCI" +
+                    "&client_secret=ZH4CRZNEWBNTALAE3INIB5XG0QI12R4DT5HKAJLWKYE1LHOG" +
+                    "&v=20160215&limit=5" +
+                    "&near=" + placeEntered +
+                    "&query=" + searchQuery);
+                handler.success(function (data) {
+
+                    if (data != null && data.response != null && data.response.venues != undefined && data.response.venues != null) {
+                        for (var i = 0; i < data.response.venues.length; i++) {
+                            $scope.venueList[i] = {
+                                "name": data.response.venues[i].name,
+                                "id": data.response.venues[i].id,
+                                "location": data.response.venues[i].location
+                            };
+                        }
+                    }
+
+                })
+                handler.error(function (data) {
+                    alert("There was some error processing your request. Please try after some time.");
+                });
+            }
+        }
+
+        $scope.getaudio = function(ddesc12)
+        {
+
+            var SpeechUrl='https://stream.watsonplatform.net/text-to-speech/api/v1/synthesize?username=adde4405-d282-474e-9e33-42a61f8cc777&password=l62ntQRZxvqw&text='+ddesc12;
+
+
+            document.getElementById("Audio").innerHTML= "<video controls='' autoplay='' name='media'><source src='"+SpeechUrl+"' type='audio/ogg'></video>";
+        }
+    });
